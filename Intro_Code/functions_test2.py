cae3777576c96f02d7450223d38536dc805c4026
yasirsheikh07 <yasirsheikh844@gmail.com>
2025-08-16 17:14:59 +0530

    Add files via upload

---
+++ Intro_Code/functions_test2.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Functions in python
+
+
+# In[5]:
+
+
+def add2numbers(a, b): #Creating a Function 
+    output = a + b
+    print("The addition of 2 numbers:", output)
+
+
+# In[6]:
+
+
+add2numbers(50, 59) #Calling a Function
+
+
+# In[7]:
+
+
+def multiplication2numbers(a, b): 
+    output = a * b
+    print("The multiplication of 2 numbers:", output)
+
+
+# In[8]:
+
+
+multiplication2numbers(59, 50)
+
+
+# In[9]:
+
+
+def divide2numbers(a, b):
+    output = a / b
+    print("The result is:", output)
+
+
+# In[10]:
+
+
+divide2numbers(2,6)
+
+
+# In[11]:
+
+
+def sub2numbers(a, b):
+    output = a - b
+    print("The subtraction of 2 numbers:", output)
+
+
+# In[12]:
+
+
+sub2numbers(2,5)
+
+
+# In[13]:
+
+
+def sqaureofnumber(a):
+    output = a * a
+    print("The Square your given number:", output)
+
+
+# In[14]:
+
+
+sqaureofnumber(25)
+
+
+# In[15]:
+
+
+def floordivide2numbers(a, b):
+    output = a // b
+    print("The floor_division of 2 numbers:", output)
+
+
+# In[16]:
+
+
+floordivide2numbers(7, 2)
+
+
+# In[17]:
+
+
+Y = 1
+D = 2
+
+
+# In[20]:
+
+
+multiplication2numbers(Y, D)
+
+
+# In[21]:
+
+
+multiplication2numbers(1,2)
+
+
+# In[22]:
+
+
+def add3numbers(a,b,c):
+    return a+b+c
+
+
+# In[23]:
+
+
+one = add3numbers(1,2,1003)
+print(one)
+
+
+# In[24]:
+
+
+print(3 ** 3)
+
+
+# In[3]:
+
+
+def function_to_used_in_future():
+    pass
+print("Hello! you used pass in this function")
+
+
+# In[4]:
+
+
+def add4numbers(a, b, c, d):
+    output = a + b + c + d
+    return output
+
+
+# In[8]:
+
+
+input1 = add4numbers(1, 100, 200, 5)
+print(input1)
+
+
+# In[9]:
+
+
+def multi5numbers(a, b, c, d, e):
+    output = a * b * c * d * e
+    return output
+
+
+# In[13]:
+
+
+input2 = multi5numbers(1, 200, 500, 600, 5)
+print(input2)
+
+
+# In[17]:
+
+
+def function_to_used_in_future2():
+    pass
+
+def add3numbers(a, b, c):
+    output = a + b + c
+    return output
+
+input3 = add3numbers(200, 1000, 5986)
+print(input3)
+
+
+# In[20]:
+
+
+input4 = add3numbers(200, 1000, 5985)
+print(input4)
+
+
+# In[ ]:
+
+
+
+
